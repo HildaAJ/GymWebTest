d9a4f2df36b1708215a9ecbf2de8a465200e49f9
陳雅蓁 <ajchen@ptc-nec.com.tw>
2019-07-28 17:25:35 +0800

    7/28 改congiguration <Gym.Models.GymEntity>

--- Gym/Migrations/Configuration.cs
+++ Gym/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace Gym.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Gym.Models.GymDBModel>
+    internal sealed class Configuration : DbMigrationsConfiguration<Gym.Models.GymEntity>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Gym.Models.GymDBModel context)
+        protected override void Seed(Gym.Models.GymEntity context)
         {
             //  This method will be called after migrating to the latest version.
 
