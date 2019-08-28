fecc56881b938ebbf892cb2ca6c5fcb572471548
陳雅蓁 <sadaco0829@gmail.com>
2019-08-28 20:34:39 +0800

    登入角色權限頁面顯示

--- Gym/Controllers/HomeController.cs
+++ Gym/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Gym.Models.ViewModels;
 using Gym.Models;
 using System.Threading.Tasks;
 using System.Web.Security;
+using Gym.Filters;
 using Gym.Models.Operation;
 
 namespace Gym.Controllers
@@ -77,23 +78,43 @@ namespace Gym.Controllers
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
         public ActionResult Login(LoginViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-            MemberDataOperation memberDataOperation = new MemberDataOperation();
+        { 
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+                MemberDataOperation memberDataOperation = new MemberDataOperation();
+                RoleDataOperation roleDataOperation = new RoleDataOperation();
+
                 if (memberDataOperation.CheckUserData(model))
                 {
+                    //取得登入會員的角色編號
+                    var tmpRole = from c in memberDataOperation.Get()
+                                  where model.Email == c.Email
+                                  select c.Role_No;
+                    int RoleNo = 0;
+                    foreach (var item in tmpRole)
+                    {
+                        RoleNo = item;
+                    }
+
+                    //取得會員的角色名稱
+                    var RoleCollection = roleDataOperation.Get().Where(a => a.RoleNo == RoleNo).Select(a => a.Name);
+                    string Role = "";
+                    foreach (string item in RoleCollection)
+                    {
+                        Role = item;
+                    }
+
                     var ticket = new FormsAuthenticationTicket(
                      version: 1,
-                     name: memberDataOperation.user.Email.ToString(), //可以放使用者Id
+                     name: memberDataOperation.user.Email.ToString(), 
                      issueDate: DateTime.UtcNow,//現在UTC時間
                      expiration: DateTime.UtcNow.AddMinutes(30),//Cookie有效時間=現在時間往後+30分鐘
                      isPersistent: false,// 是否要記住我 true or false
-                     userData: memberDataOperation.user.Name,  //可以放使用者角色名稱
+                     userData: Role+","+ memberDataOperation.user.Name, 
                      cookiePath: FormsAuthentication.FormsCookiePath);
 
                     var encryptedTicket = FormsAuthentication.Encrypt(ticket); //把驗證的表單加密
@@ -106,7 +127,7 @@ namespace Gym.Controllers
                     };
 
                     Response.Cookies.Add(cookie);
-                    ViewBag.Name = memberDataOperation.user.Name;
+                    //ViewBag.UserName = memberDataOperation.user.Name;
                     return RedirectToAction("Index", "Home");
                 }
                 else
@@ -127,6 +148,9 @@ namespace Gym.Controllers
         public ActionResult Logout()
         {
             FormsAuthentication.SignOut();
+            //清除所有的 session
+            Session.RemoveAll();
+
             ViewBag.Name = "Guest";
             return RedirectToAction("Index");
         }
